dfbeb70ae66a9f65fdd7651881cc8c39cc73e7c0
Adliano Alves <adliano@me.com>
2019-04-06 00:57:54 -0700

    finish the timer picker and minute picker

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,3 +1,8 @@
+// https://github.com/weareoutman/clockpicker
+// file:///Users/adliano/Downloads/clockpicker-gh-pages/index.html
+
+
+
 /********* Global Variables ********/
 let trainInfo = {
     // get get-train-name
@@ -5,16 +10,19 @@ let trainInfo = {
     // get get-destination
     destination: "",
     // get get-train-time
-    time: "",
+    time: {
+        hour: 0,
+        minutes: 0,
+    },
     // get get-frequncy
     fequency: "",
 };
 
 let inputElements = {
-    name : document.querySelector("#get-train-name"),
-    destination : document.querySelector("#get-destination"),
-    time : document.querySelector("#get-train-time"),
-    fequency : document.querySelector("#get-frequency"),
+    name: document.querySelector("#get-train-name"),
+    destination: document.querySelector("#get-destination"),
+    time: document.querySelector("#get-train-time"),
+    fequency: document.querySelector("#get-frequency"),
 }
 // Firebase configurations
 
@@ -26,31 +34,65 @@ let inputElements = {
 
 
 function onSubmitClick() {
-    
+
     // get get-train-name
     trainInfo.name = inputElements.name.value;
     // get get-destination
     trainInfo.destination = inputElements.destination.value;
-    // get get-train-time
-    trainInfo.time = inputElements.time.value;
+
     // get get-frequncy
-    trainInfo.fequency = inputElements.fequency.value;
+    //trainInfo.fequency = inputElements.fequency.value;
     // clean the inputs
-    for(let _key in inputElements) {
+    for (let _key in inputElements) {
         inputElements[_key].value = "";
     }
 
     console.dir(trainInfo);
 }
 
-function onTimeEnter(){
-    console.log(inputElements.time.value);
+function onTimeEnter() {
+    // get get-train-time
+    let _strTime = inputElements.time.value;
+    trainInfo.time.hour = parseInt(_strTime.split(':')[0]);
+    trainInfo.time.minutes = parseInt(_strTime.split(':')[1]);
+
+    console.dir(trainInfo);
+
+}
+
+function onFrequencySet() {
+    inputElements.fequency.value = inputElements.fequency.value.split(':')[1];
+    trainInfo.fequency = parseInt(inputElements.fequency.value);
 }
 
 
-
-
-
+/*****************************************************/
+/******************* Timepicker **********************/
+/*****************************************************/
+$(document).ready(function () {
+    $('#get-train-time').clockpicker({
+        placement: 'top',
+        align: 'left',
+        // auto close when minute is selected
+        autoclose: true,
+        // callback function triggered after time is written to input
+        afterDone: onTimeEnter,
+    });
+    // Settings for Minute Picker
+    let minutePicker = $('#get-frequency').clockpicker({
+        // auto close when minute is selected
+        autoclose: true,
+        placement: 'top',
+        align: 'left',
+        // callback function triggered after time is written to input
+        afterDone: onFrequencySet,
+    });
+    // Call Minute picker when input have the focus
+    $('#get-frequency').focusin(function () {
+        minutePicker.clockpicker('show')
+            .clockpicker('toggleView', 'minutes');
+    });
+});
 
 
 inputElements.time.addEventListener(`input`, onTimeEnter);
