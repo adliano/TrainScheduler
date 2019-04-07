66a687793079a53f74cc2c8054a6d7143b1f0538
Adliano Alves <adliano@me.com>
2019-04-06 22:27:36 -0700

    add firebase

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -14,39 +14,80 @@ let trainInfo = {
         hour: 0,
         minutes: 0,
     },
-    // get get-frequncy
-    fequency: "",
+    // get get-frequency
+    frequency: "",
 };
 
 let inputElements = {
     name: document.querySelector("#get-train-name"),
     destination: document.querySelector("#get-destination"),
     time: document.querySelector("#get-train-time"),
-    fequency: document.querySelector("#get-frequency"),
+    frequency: document.querySelector("#get-frequency"),
 }
 // Firebase configurations
+let config = {
+    apiKey: "AIzaSyDwsaSFbMU9Iy7mLVkEB_EMXh2O5PGoGf8",
+    authDomain: "demoprojectfb-d2812.firebaseapp.com",
+    databaseURL: "https://demoprojectfb-d2812.firebaseio.com",
+    projectId: "demoprojectfb-d2812",
+    storageBucket: "demoprojectfb-d2812.appspot.com",
+    messagingSenderId: "594142064753"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
 
-// init firebase database
+
+let database = firebase.database();
 
 // get data from firebase and update info on set-train-info
 
 // ------ get user data using JSON -------
 
+function isDataMissing(){
+    // Loop through each key in JSON
+    for(let _key in trainInfo){
+        // get the value for the current key
+        let _data = trainInfo[_key];
+        // check if data is available
+        if(_data.length < 1){
+            // if no data return true
+            return true;
+        }
+    }
+    // if all data is populated return false
+    return false;
+}
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~ onSubmitClick() ~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 function onSubmitClick() {
     // get get-train-name
     trainInfo.name = inputElements.name.value;
     // get get-destination
     trainInfo.destination = inputElements.destination.value;
+    // Check if all required fields are populated
+    if (!isDataMissing()){
+        // Push data to firebase
+        database.ref().push(trainInfo);
+        // clean the inputs
+        for (let _key in inputElements) {
+            inputElements[_key].value = "";
+        }
 
-    // clean the inputs
-    for (let _key in inputElements) {
-        inputElements[_key].value = "";
     }
+    // if any missing data display error
+    else {
+        // TODO display error if missing data
+        alert("Missing data");
+    }
+
 
     console.dir(trainInfo);
 }
-
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~ onTimeEnter() ~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 function onTimeEnter() {
     // get get-train-time
     let _strTime = inputElements.time.value;
@@ -58,8 +99,8 @@ function onTimeEnter() {
 }
 
 function onFrequencySet() {
-    inputElements.fequency.value = inputElements.fequency.value.split(':')[1];
-    trainInfo.fequency = parseInt(inputElements.fequency.value);
+    inputElements.frequency.value = inputElements.frequency.value.split(':')[1];
+    trainInfo.frequency = parseInt(inputElements.frequency.value);
 }
 
 /*****************************************************/
@@ -90,6 +131,8 @@ $(document).ready(function () {
     });
 });
 
-
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 inputElements.time.addEventListener(`input`, onTimeEnter);
 document.querySelector("#btn-submit").addEventListener("click", onSubmitClick);
